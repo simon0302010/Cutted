041b1d4c9994a5aad43ff4266b2eeed0420df639
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-06-25 16:57:31 +0200

    added slider to select point in audio

--- cutted/app.py
+++ cutted/app.py
@@ -7,6 +7,8 @@ from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
                                                NavigationToolbar2Tk)
 from matplotlib.figure import Figure
 
+customtkinter.set_appearance_mode("Dark")
+
 class CuttedApp:
     def __init__(self):
         self.AudioProcessor = audio_processor.AudioProcessor()
@@ -18,8 +20,14 @@ class CuttedApp:
         self.root.title("Cutted")
         self.root.geometry("600x400")
         
-        self.plot_frame = customtkinter.CTkFrame(self.root, height=250)
-        self.plot_frame.pack(side=customtkinter.TOP, fill=customtkinter.X, padx=10, pady=10)
+        self.plot_frame = customtkinter.CTkFrame(self.root, height=250, fg_color="transparent")
+        self.plot_frame.pack(
+            side=customtkinter.TOP, 
+            fill=customtkinter.BOTH, 
+            expand=True, 
+            padx=20,
+            pady=(20, 150),
+        )
         self.plot_frame.pack_propagate(False)
         
         button = customtkinter.CTkButton(self.root, text="Load audio", command=self.select_file)
@@ -32,6 +40,10 @@ class CuttedApp:
         if file_path:
             self.AudioProcessor.load_audio(file_path)
             self.update_plot()
+            
+    def set_cursor(self, value):
+        value = round(value)
+        print(f"Slider Value: {value}")
 
     def update_plot(self):
         if self.canvas:
@@ -40,7 +52,20 @@ class CuttedApp:
         fig = self.AudioProcessor.plot_audio()
         self.canvas = FigureCanvasTkAgg(fig, master=self.plot_frame)
         self.canvas.draw()
-        self.canvas.get_tk_widget().pack(fill=customtkinter.BOTH, expand=True)
+        
+        self.audio_lenght = int(round(self.AudioProcessor.get_lenght()))
+        self.slider = customtkinter.CTkSlider(
+            self.root, from_=0, to=self.audio_lenght, command=self.set_cursor, width=500
+        )
+        
+        self.slider.place(relx=0.5, rely=1.0, anchor="s", y=-130)
+        
+        self.canvas.get_tk_widget().pack(
+            fill=customtkinter.BOTH, 
+            expand=True, 
+            padx=10,
+            pady=10,
+        )
     
     def run(self):
         self.root.mainloop()
