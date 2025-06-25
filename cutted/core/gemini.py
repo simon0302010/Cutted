668e5662076fb5e8b304fe4c18d059823a3e2cf1
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-06-25 22:35:11 +0200

    beginning to implement gemini

--- cutted/core/gemini.py
+++ cutted/core/gemini.py
@@ -1,2 +1,8 @@
+import os
 from google import genai
 from google.genai import types
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+if not GEMINI_API_KEY:
+    print("Please set the environment variable GEMINI_API_KEY to your Gemini API Key.")
