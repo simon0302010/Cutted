bd2e99f32b35e4c7381f5e2efe6f5248e8a4a2be
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-06-25 14:01:56 +0200

    added function to plot audio

--- cutted/core/audio_processor.py
+++ cutted/core/audio_processor.py
@@ -1,1 +1,36 @@
-import pydub
+from pydub import *
+from pydub.utils import ratio_to_db
+from .logger import *
+import numpy as np
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
+                                               NavigationToolbar2Tk)
+from matplotlib.figure import Figure
+
+class AudioProcessor:
+    def __init__(self):
+        self.audio_path = None
+    
+    def load_audio(self, audio_path: str, volume: float = 1.0):
+        self.audio_path = audio_path
+        self.audio = AudioSegment.from_file(self.audio_path)
+        self.audio = self.audio.apply_gain(ratio_to_db(volume))
+        print_info(f"Loaded {self.audio_path}")
+        
+    def plot_audio(self, save_path=None):
+        if self.audio is None:
+            print_fail("No audio loaded.")
+            return
+        
+        samples = np.array(self.audio.get_array_of_samples())
+        if self.audio.channels == 2:
+            samples = samples.reshape((-1, 2))
+            samples = samples.mean(axis=1)
+            
+        times = np.linspace(0, len(samples) / self.audio.frame_rate, num=len(samples))
+        
+        fig = Figure(figsize=(5, 4))
+        ax = fig.add_subplot()
+        line, = ax.plot(times, samples)
+        
+        return fig
