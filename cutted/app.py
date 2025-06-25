fd6c9ea73b4cea07a353e914be2196d51fedc13d
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-06-25 10:39:58 +0200

    added basic UI

--- cutted/app.py
+++ cutted/app.py
@@ -1,2 +1,17 @@
+import customtkinter
+
+def load_file():
+    file_path = customtkinter.filedialog.askopenfilenames(
+        filetypes=[("Audio files", ".mp3 .wav .aac .flac .ogg .m4a")]
+    )
+    print(list(file_path))
+
 def main():
-    print("Hello World!")
+    root = customtkinter.CTk()
+    root.title("Cutted")
+    root.geometry("600x400")
+
+    button = customtkinter.CTkButton(root, text="Load audio", command=load_file)
+    button.pack()
+
+    root.mainloop()
