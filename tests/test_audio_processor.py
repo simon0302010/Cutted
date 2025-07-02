4d61df074e9028cd08493764fd97ac07d1c4c278
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-07-02 21:53:45 +0200

    reformatted code

--- tests/test_audio_processor.py
+++ tests/test_audio_processor.py
@@ -1,5 +1,7 @@
 import pytest
+
 from cutted.core.audio_processor import AudioProcessor
+
 
 @pytest.fixture
 def audio_processor():
@@ -7,16 +9,20 @@ def audio_processor():
     processor.load_audio("tests/test_audio.mp3")
     return processor
 
+
 def test_load_audio(audio_processor):
     assert audio_processor.audio is not None
 
+
 def test_get_length(audio_processor):
     assert 29.9 < audio_processor.get_length() < 30.1
+
 
 def test_cut(audio_processor):
     original_length = audio_processor.get_length()
     audio_processor.cut([5], [10])
     assert audio_processor.get_length() < original_length
+
 
 def test_change_volume(audio_processor):
     original_audio = audio_processor.audio
